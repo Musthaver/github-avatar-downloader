6bf0945ad1cbde6bcf36174184eb3c6543d711ef
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-06 22:27:09 +0000

    added functioning downloadImageURL when hardcoded

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secret = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,17 +9,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
             'User-Agent': 'request',
-            "Authorization": secret.GITHUB_TOKEN
+            'Authorization': secret.GITHUB_TOKEN
         }
-      };
+    };
 
-request(options, function(err, res, body) {
-    cb(JSON.parse(body));
-  });
+    request(options, function(err, res, body) {
+        if (err) throw err;
+        cb(JSON.parse(body));
+    });
 }
+
 var getImageURLs = function(Array) {
     for (i = 0; i < Array.length; i++) {
         console.log(Array[i].avatar_url)
     }
 }
-getRepoContributors("jquery", "jquery", getImageURLs);
+
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .on('response', function (response) {                           
+        console.log(response.headers);
+      })
+        .pipe(fs.createWriteStream(filePath));
+}
+
+// getRepoContributors("jquery", "jquery", getImageURLs);
+
+downloadImageByURL("https://avatars0.githubusercontent.com/u/1615?v=4", './downloaded.jpeg')
